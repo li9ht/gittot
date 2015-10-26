8a4768f205089ec09226c72a332f0ae7ad85f86a
Rizal <rizalyunus@ukm.edu.my>
2015-10-26 12:06:42 +0800

    new commit

---
+++ K017673/calculator.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Calculator{
+
+	$result=0;
+
+	function add(a,b)
+	{
+		$this->result = a + b;
+	}
+
+	function showResult(){
+		echo $this->result;
+	}
+
+}
+
+/* End of file calculator.php */
+/* Location: .//C/Users/rizal/Desktop/gittot1/calculator.php */
