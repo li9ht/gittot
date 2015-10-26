0eec81fedc82c80a6fdb98026256cbe60aa42c77
diana <greenrose_dd@yahoo.com>
2015-10-26 11:50:58 +0800

    commit 2 file

---
+++ K018396/calculator.php
@@ -0,0 +1,20 @@
+<?php 
+
+class calculator {
+
+	$result=0;
+
+	function add(a,b) {
+		$this->result = a + b;
+	}
+
+	function showResult() {
+		echo $this->result;
+	}
+
+	function minus(a,b) {
+	$this->result = a - b;
+}
+  }
+
+ ?>
